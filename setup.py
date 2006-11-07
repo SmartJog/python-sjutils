640ec16a11e7e0ed3d82b3ebc15d566b38e4e296
Laurent Corbes <laurent.corbes@smartjog.com>
2006-11-07 13:44:23 +0000

    python module build system debian cdbs+pycentral new policy

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+##
+## Login : <lcorbes@smartjog.com>
+## Started on  Tue Nov  7 14:24:30 2006 Laurent Corbes
+##
+## $Rev::                                           $:  Revision of last commit
+## $Author::                                        $:  Author of last commit
+## $Date::                                          $:  Date of last commit
+
+from distutils.core import setup
+
+
+setup (
+    packages=['sjutils']
+    )
