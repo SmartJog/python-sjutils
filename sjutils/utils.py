545cbe4a6bc782ac6b46885e536e288e5e35c732
Philippe Bridant <philippe.bridant@smartjog.com>
2007-11-29 18:04:31 +0000

    section not mandatory for logger

--- sjutils/utils.py
+++ sjutils/utils.py
@@ -1,14 +1,15 @@
 import os, string, dircache, time, sys, md5, sha, re
 from htmlentitydefs import entitydefs
 
-def ismd5(DIR):
+def ismd5(md5):
         hex="0123456789abcdef"
-        if len(DIR) != 32:
+        if len(md5) != 32:
                 return False
-        for a in DIR:
+        for a in md5:
                 if string.find(hex, a) == -1:
                         return False
         return True
+
 
 def md5sum(f):
 	f = file(f)
@@ -75,14 +76,17 @@ class Logger:
         _file = ""
         _label = ""
         
-        def __init__(self, logfile, label):
+        def __init__(self, logfile, label=None):
                 self._file = open(logfile, "a", 1)
-                self._label = label
+                if label:
+                    self._label = "[%s]" % label
+                else:
+                    self.label = ""
                 
         def write(self, str):
                 t = time.gmtime()
                 ts = "%02d/%02d/%02d GMT %02d:%02d:%02d " % (t[2], t[1], t[0], t[3], t[4], t[5])
-                self._file.write(ts + "[" + self._label + "] " + str + "\n")
+                self._file.write(ts + self._label + str + "\n")
 
         def redirect_stdout_stderr(self):
                 sys.stdout = sys.stderr = self._file
