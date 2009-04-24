22759006fa1d77dabec927b23a93127d0c795f7f
Nicolas Noirbent <nicolas.noirbent@smartjog.com>
2009-04-24 17:40:15 +0200

    Move DefaultDict to python-sjutils

--- sjutils/utils.py
+++ sjutils/utils.py
@@ -150,3 +150,17 @@ class Logger2:
 
     def close(self):
         self._file.close()
+
+class DefaultDict(dict):
+    """Trivial DefaultDict implementation. Basically returns the
+    default value for missing keys.
+
+    TODO: Python2.5 has __missing__ for dict subclasses, we should use
+    this (or collections.defaultdict) instead when switching to 2.5"""
+
+    def __init__(self, default = None, *args, **kw):
+        dict.__init__(self, *args, **kw)
+        self.default = default
+
+    def __getitem__(self, key):
+        return self.get(key, self.default)
