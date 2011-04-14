71d80a233e6c8ca03c271b684b854047de7c9f46
Gilles Dartiguelongue <gilles.dartiguelongue@smartjog.com>
2011-04-14 17:29:09 +0200

    Add logging capacity to PgConnManager

--- sjutils/pgconnmanager.py
+++ sjutils/pgconnmanager.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import logging
 import threading
 
 import psycopg2
@@ -64,6 +65,11 @@ class PgConnManager(object):
     def __new__(self, *kargs, **kwargs):
         db_opts = kargs[0]
 
+        if 'logger' in kwargs:
+            logger = kwargs.pop('logger')
+        else:
+            logger = logging.getLogger('sjutils.pgconnmanager')
+
         # We can either accept 'database' or 'dbname' as an input
         if db_opts.has_key('database') and not db_opts.has_key('dbname'):
             db_opts['dbname'] = db_opts['database']
@@ -77,6 +83,8 @@ class PgConnManager(object):
         if not self._instances.has_key(db_str):
             self._instances[db_str] = super(PgConnManager, self).__new__(self, *kargs, **kwargs)
             self._instances[db_str].lock = threading.Lock()
+            self._instances[db_str].log = logger
+
         return self._instances[db_str]
 
     def __init__(self, db_opts):
@@ -112,14 +120,18 @@ class PgConnManager(object):
             _, value, traceback = sys.exc_info()
             raise self.DatabaseError, value, traceback
 
-    def _new_ctx(self):
+    def _new_ctx(self, mark = None):
         """ Create a new context object. """
-        ret = { 'conn' : None, 'cursor' : None }
+        if not mark:
+            mark = 'none'
+        ret = { 'conn' : None, 'cursor' : None, 'mark': mark }
         ret['conn'] = self.__conn_pool__.getconn()
+        self.log.debug('ctx:(' + mark + ', ' + str(id(ret)) + ') Creating context to database: %(dbname)s as %(user)s' % self.__params__)
         return ret
 
-    def connect(self, ctx_list = None):
+    def connect(self, ctx_list = None, mark = None):
         """ Connect to database. """
+        self.log.debug('Connecting to database: %(dbname)s as %(user)s' % self.__params__)
         try:
             self.lock.acquire_lock()
             try:
@@ -130,7 +142,13 @@ class PgConnManager(object):
                 self.lock.release_lock()
             if not ctx_list:
                 ctx_list = []
-            ctx_list.append(self._new_ctx())
+            ctx_list.append(self._new_ctx(mark))
+            self.log.debug('ctx:(%(mark)s, %(ctxid)s) Connected to database: %(dbname)s as %(user)s' % {
+                'mark': ctx_list[-1]['mark'],
+                'ctxid': str(id(ctx_list[-1])),
+                'dbname': self.__params__['dbname'],
+                'user': self.__params__['user'],
+            })
             return ctx_list
 
         except psycopg2.Error, _error:
@@ -141,6 +159,12 @@ class PgConnManager(object):
 
     def execute(self, ctx, query, options=None):
         """ Execute an SQL query. """
+        self.log.debug('ctx:(%(mark)s, %(ctxid)s) Executing query on database: %(dbname)s as %(user)s' % {
+            'mark': ctx['mark'],
+            'ctxid': str(id(ctx)),
+            'dbname': self.__params__['dbname'],
+            'user': self.__params__['user'],
+        })
         try:
             if not ctx['conn']:
                 ctx['conn'] = self.__conn_pool__.getconn()
@@ -163,6 +187,12 @@ class PgConnManager(object):
 
     def commit(self, ctx):
         """ Commit changes to dabatase. """
+        self.log.debug('ctx:(%(mark)s, %(ctxid)s) Commiting changes on database: %(dbname)s as %(user)s' % {
+            'mark': ctx['mark'],
+            'ctxid': str(id(ctx)),
+            'dbname': self.__params__['dbname'],
+            'user': self.__params__['user'],
+        })
         try:
             try:
                 ctx['conn'].commit()
@@ -178,39 +208,77 @@ class PgConnManager(object):
 
     def rollback(self, ctx):
         """ Rollback changes to database. """
+        self.log.debug('ctx:(%(mark)s, %(ctxid)s) Reverting changes on database: %(dbname)s as %(user)s' % {
+            'mark': ctx['mark'],
+            'ctxid': str(id(ctx)),
+            'dbname': self.__params__['dbname'],
+            'user': self.__params__['user'],
+        })
         # FIXME: ctx['conn'] is None if execute failed, see bug #3085
         if ctx['conn'] and not ctx['conn'].closed:
             ctx['conn'].rollback()
 
     def release(self, ctx, rollback=False, close=False):
         """ Release database connection. """
+        self.log.debug('ctx:(%(mark)s, %(ctxid)s) Releasing connection on database: %(dbname)s as %(user)s' % {
+            'mark': ctx['mark'],
+            'ctxid': str(id(ctx)),
+            'dbname': self.__params__['dbname'],
+            'user': self.__params__['user'],
+        })
 
         if rollback:
             self.rollback(ctx)
 
         ctx['cursor'] = None
         if ctx['conn']:
+            self.log.debug('ctx:(%(mark)s, %(ctxid)s) Disposing connection on database: %(dbname)s as %(user)s' % {
+                'mark': ctx['mark'],
+                'ctxid': str(id(ctx)),
+                'dbname': self.__params__['dbname'],
+                'user': self.__params__['user'],
+            })
             closeit =  close or (ctx['conn'].closed > 0)
             self.__conn_pool__.putconn(ctx['conn'], close=closeit)
         ctx['conn'] = None
 
     def release_all(self, ctx_list, rollback=False, close=False):
         """ Release all database connections from a context list. """
+        self.log.debug('Releasing ALL connections on database: %(dbname)s as %(user)s' % self.__params__)
+
         for ctx in ctx_list:
             self.release(ctx, rollback=rollback, close=close)
         ctx_list = None
 
     def fetchall(self, ctx):
         """ Return all rows of current request. """
+        self.log.debug('ctx:(%(mark)s, %(ctxid)s) fetchall on database: %(dbname)s as %(user)s' % {
+            'mark': ctx['mark'],
+            'ctxid': str(id(ctx)),
+            'dbname': self.__params__['dbname'],
+            'user': self.__params__['user'],
+        })
         return ctx['cursor'].fetchall()
 
     def fetchmany(self, ctx, arraysize=1000):
         """ Return @arraysize rows of current request. """
+        self.log.debug('ctx:(%(mark)s, %(ctxid)s) fetchmany on database: %(dbname)s as %(user)s' % {
+            'mark': ctx['mark'],
+            'ctxid': str(id(ctx)),
+            'dbname': self.__params__['dbname'],
+            'user': self.__params__['user'],
+        })
         return ctx['cursor'].fetchmany(arraysize)
 
     def fetchgenerator(self, ctx):
         """ A basic generator to iterate through the current request's
         result rows. """
+        self.log.debug('ctx:(%(mark)s, %(ctxid)s) fetchgenerator on database: %(dbname)s as %(user)s' % {
+            'mark': ctx['mark'],
+            'ctxid': str(id(ctx)),
+            'dbname': self.__params__['dbname'],
+            'user': self.__params__['user'],
+        })
         while True:
             results = self.fetchmany(ctx)
             if not results:
@@ -224,4 +292,10 @@ class PgConnManager(object):
 
     def fetchone(self, ctx):
         """ Return one row of current request. """
+        self.log.debug('ctx:(%(mark)s, %(ctxid)s) fetchone on database: %(dbname)s as %(user)s' % {
+            'mark': ctx['mark'],
+            'ctxid': str(id(ctx)),
+            'dbname': self.__params__['dbname'],
+            'user': self.__params__['user'],
+        })
         return ctx['cursor'].fetchone()
